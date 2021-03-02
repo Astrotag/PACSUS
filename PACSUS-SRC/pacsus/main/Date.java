ff1da4c1062666cfd39b6bc22f80283033eeab20
Joanes-Iri <joi00009@students.stir.ac.uk>
2021-03-02 16:03:00 +0100

    Change back because I didn't request pull
    Sorry

--- PACSUS-SRC/pacsus/main/Date.java
+++ PACSUS-SRC/pacsus/main/Date.java
@@ -24,37 +24,17 @@ package pacsus.main;
  */
 public class Date
 {
-	/**
-     * For simplicity, dates will be represented just by an integer in the range
-     * 1-365. No Leap years!
-     */
-    private int dayNumber;
-    
-    public Date() {
-    	dayNumber = 1;
-    }
-    
     /**
      * This operation adds one to the day number, wrapping round to 1 after 365
      * automatically.
      */
     public void increment()
     {
-    	// TODO: develop further (Mind the range 1-365)
-    	if (dayNumber>365) {
-    		dayNumber = 1;
-    	} else {
-    		dayNumber++;
-    	}
-    		
     }
-    
+
     /**
-     * 
-     * @return dayNumber 
+     * For simplicity, dates will be represented just by an integer in the range
+     * 1-365. No Leap years!
      */
-    public int getDayNumber() {
-    	return dayNumber;
-    }
-    
+    private int dayNumber;
 }
