3b9ebcd2b341630d4751ffa19a5bf2d816b3d40d
Joanes-Iri <joi00009@students.stir.ac.uk>
2021-03-04 12:28:45 +0100

    Timer Interface (Basic)
    Timer.java changed for UI:
    Implemented button to send signal for "new day"
    Implemented label to display the current day.
    Date.java changed:
    Implemented a getter for dayNumber attribute
    Increment() method developed

--- PACSUS-SRC/pacsus/main/Date.java
+++ PACSUS-SRC/pacsus/main/Date.java
@@ -24,17 +24,37 @@ package pacsus.main;
  */
 public class Date
 {
+	/**
+     * For simplicity, dates will be represented just by an integer in the range
+     * 1-365. No Leap years!
+     */
+    private int dayNumber;
+    
+    public Date() {
+    	dayNumber = 1;
+    }
+    
     /**
      * This operation adds one to the day number, wrapping round to 1 after 365
      * automatically.
      */
     public void increment()
     {
+    	// TODO: develop further (Mind the range 1-365)
+    	if (dayNumber>365) {
+    		dayNumber = 1;
+    	} else {
+    		dayNumber++;
+    	}
+    		
     }
-
+    
     /**
-     * For simplicity, dates will be represented just by an integer in the range
-     * 1-365. No Leap years!
+     * 
+     * @return dayNumber 
      */
-    private int dayNumber;
+    public int getDayNumber() {
+    	return dayNumber;
+    }
+    
 }
