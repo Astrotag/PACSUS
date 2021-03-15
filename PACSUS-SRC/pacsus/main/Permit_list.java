d53009456d018ab66e05dc645110b4c9d7070e67
Greig <greigmmyles@hotmail.co.uk>
2021-03-15 11:32:27 +0000

    Cleaning the Code
    Removed more System.out.printlns

--- PACSUS-SRC/pacsus/main/Permit_list.java
+++ PACSUS-SRC/pacsus/main/Permit_list.java
@@ -37,7 +37,7 @@ public class Permit_list
     public Permit_list()
     {
 	lnkPermit = new Hashtable<String, Permit>();
-	System.out.println(lnkPermit.toString());
+	//System.out.println(lnkPermit.toString());
     }
 
     public boolean add(String key, Permit p)
@@ -62,8 +62,8 @@ public class Permit_list
 
 	for (int i = 0; i < keys.length; i++)
 	{
-	    System.out.println(keys[i]);
-	    System.out.println(permits[i].toString());
+	    //System.out.println(keys[i]);
+	    //System.out.println(permits[i].toString());
 	    strings[i] = "" + permits[i].toString();
 	}
 
